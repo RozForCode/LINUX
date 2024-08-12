eaf2d92c4e8a850a5cf9cfa991fd9d16ed344c49
Navrose <navrosesinghjohal@gmail.com>
2024-08-11 22:11:12 -0400

    week9 done now week8

--- LINUX/notes3.sh
+++ LINUX/notes3.sh
@@ -41,4 +41,14 @@ read yourChoice
 awk '$1==$yourChoice {print}' file
 awk 'NR==$yourChoice {print}' file #your file doesn't have numbers
 
+#arithemtic sum of all 3rd field values
+awk -F: '{total += $3} END {print total}' file
+# use of END - runs after all input is processed
+# use of BEGIN - runs before all input is processed
+
+#bash cannot calc with floating point but awk can
+awk 'BEGIN {printf "%.3f\n", 2005.03/3}' 
+
+#piping with awk
+history | awk '{print $2}' | sort | uniq -c | sort -rn | head
 
