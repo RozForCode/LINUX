82b42b541ee69f8d1b574571e28fdd362e607a16
Navrose <navrosesinghjohal@gmail.com>
2024-08-11 23:21:35 -0400

    week6 done now week 4

---
+++ LINUX/notes6.sh
@@ -0,0 +1,37 @@
+# WEEK-6 Slate modules summary/notes
+paste file1 file2 > file3
+# joins first line of file1 with first line of file2 and so on
+join file1 file2 > file3
+# joins file1 and file2 based on common field
+sort 
+:'
+-d - blanks and alphanumeric
+-f - ignores case
+-M - month
+-n - numerically
+-r - reverse order
+-K - start and end at a user-defined position
+-t - delimiter
+'
+cut -d: -f 3 company.data
+cut -c1-6 comany.data
+
+# diff - compare files 
+:'
+-a means appended 
+-c means changed 
+-d 
+< remove line
+> add line
+'
+
+# cmp - compare files byte by byte
+# comm - comapre sorted files
+wc -m file
+:'
+-l - no.of lines
+-c - count of bytes
+-m - no.of chars
+-L - length of longest line
+
+'
